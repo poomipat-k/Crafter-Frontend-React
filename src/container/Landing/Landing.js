3129e33efd11836dabd1dd962af459a6f0583bf8
poomipat-k <k.poomipat@gmail.com>
2020-10-08 20:31:42 +0700

    Update readme.md and add site introduction message on landing page

--- src/container/Landing/Landing.js
+++ src/container/Landing/Landing.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react";
+import classes from "./Landing.module.css";
 
 const Landing = (props) => {
-    return (
-        <div>
-            <h1>Landing Page</h1>
-        </div>
-
-    );
-}
+  return (
+    <div>
+        <h1 className={classes.block}>
+          Welcome to cloth online-shopping dev by Poomipat Khamai
+        </h1>
+      <div className={classes.childBlock}>
+        <h3>1. Navigate to Shop tab to see the online shop</h3>
+        <h3>2. Navagate to Design tab to see a Custom design Polo feature</h3>
+        <h3>
+          3. Navigate Cart Icon to check the current item in your cart(required
+          login)
+        </h3>
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
